d79efea1c82e42334d0a384196d8472c0fa2521c
f9lcon <twisy12@mail.ru>
2018-06-04 18:52:52 +0300

    Исправление магических чисел в задании Холст

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,13 @@ var BAR_GAP = 50;
 var BAR_Y = 245;
 var BAR_WIDTH = 40;
 var MAX_BAR = 150;
+var TITLE_X = 240;
+var TITLE_Y = 40;
+var FONT_GAP = 16;
+var COLOR_WHITE = '#fff';
+var COLOR_BLACK = '#000';
+var COLOR_GREY = 'rgba(0, 0, 0, 0.7)';
+var COLOR_RED = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -28,23 +35,23 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, COLOR_GREY);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, COLOR_WHITE);
 
   ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 240, 40);
-  ctx.fillText('Список результатов:', 225, 60);
+  ctx.fillStyle = COLOR_BLACK;
+  ctx.fillText('Ура вы победили!', TITLE_X, TITLE_Y);
+  ctx.fillText('Список результатов:', TITLE_X - FONT_GAP, TITLE_Y + FONT_GAP);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = COLOR_BLACK;
     ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP);
     ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, BAR_Y - (MAX_BAR * times[i] / maxTime) - GAP);
 
     if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillStyle = COLOR_RED;
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255,' + Math.random() + ')';
     }
